07cbeb3310d3186a169590031c1f14e5588f7725
Jesper Niemann <jesp23c6@tietgen.dk>
2020-04-29 10:04:53 +0200

    [Feature] added template for index.php, cleaned up some code.

---
+++ javascript/map.js
@@ -0,0 +1,7 @@
+var mymap = L.map('map').setView([55.781582, 10.574432], 7);
+
+var OpenStreetMap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        });
+    OpenStreetMap.addTo(mymap);
