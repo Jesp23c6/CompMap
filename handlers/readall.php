9c3dd8c6e898c94e860ae9403e25443134c9388e
Jesper Niemann <jesp23c6@tietgen.dk>
2020-05-06 12:26:49 +0200

    [WIP] working on update.php

--- handlers/readall.php
+++ handlers/readall.php
@@ -6,13 +6,15 @@ $db = new SysTem\SysDB();
 $query = $db->get_results("SELECT * FROM `firma` LEFT JOIN post ON firma.postnr = post.postnr");
 
 $result = array();
-
+/*
 while($row = $query->fetch_assoc()){
     $result[] = $row;
 }
 
 print(json_encode($result));
+*/
 
+var_dump($query);
 
 //header("location: ../index.php");
 
--- handlers/update.php
+++ handlers/update.php
@@ -3,8 +3,22 @@ include("../header.php");
 
 $db = new SysTem\SysDB();
 
+$get_array = array();
 
+$temp_array = array();
 
-header("location: index.php");
+foreach($_GET as $key => $value){
+
+    $temp_array = array($key => $value);
+
+    $get_array = $get_array + $temp_array;
+
+}
+
+var_dump($get_array);
+
+//$query = $db->update("firma", $value_array, $where_array);
+
+//header("location: ../index.php");
 
 ?>
